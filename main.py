5332ffca1088a93116ec1d6470f550f8f422f5de
El_Reng <leonelrojo7@gmail.com>
2026-04-20 19:27:18 -0300

    Práctico 1: Crud en memoria con FastAPI

---
+++ main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from fastapi import Body
+
+app = FastAPI()
+app.title = "API - Rojo Leonel"
+app.summary = "Práctico 1: Crud en memoria con FastAPI"
+
+recordatorios = [
+    {"id": 1, "titulo": "Tarea de Backend", "fecha": "22/04"},
+    {"id": 2, "titulo": "Tarea de Frontend", "fecha": "22/04"},
+    {"id": 3, "titulo": "Ver ultima clase de Diseño", "fecha": "23/04"},
+]
+
+@app.get("/recordatorios")
+async def ver_recordatorios():
+    return recordatorios
+
+@app.get("/recordatorio/{id}")
+def ver_recodatorio(id: int):
+    for r in recordatorios:
+        if r["id"] == id:
+           return r
+    return []
+
+@app.post("/agregar-recordatorio")
+def agregar_recordatorio(id: int, titulo: str, fecha: str):
+    recordatorio_nuevo = {"id": id, "titulo": titulo, "fecha": fecha} 
+    recordatorios.append(recordatorio_nuevo)
+    return recordatorios
+
+@app.put("/reescribir-recordatorio")
+def reescribir_recordatorio(id: int = Body(), titulo: str = Body(), fecha: str = Body()):
+    for r in recordatorios:
+        if r["id"] == id:
+            r["titulo"] = titulo
+            r["fecha"] = fecha
+            return {"msg": "El recordatorio fue editado correctamente"}
+    return []
+
+@app.patch("/editar-recordatorio")
+def editar_recordatorio(id: int, titulo: str | None = None, fecha: str | None = None):
+    for r in recordatorios:
+        if r["id"] == id:
+            r["titulo"] = titulo if titulo else r["titulo"]
+            r["genero"] = fecha if fecha else r["genero"]
+            return r
+    return {"msg": "No se encontró el recordatorio a editar"}
+
+@app.delete("/borrar-recordatorio/{id}")
+def borrar_recordatorio(id: int):
+    for r in recordatorios:
+        if r["id"] == id:
+            recordatorios.remove(r)
+            return recordatorios
+    return {"msg": "Recordatorio no encontrado"}
