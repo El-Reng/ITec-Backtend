c72f354f6634b2e013e439ff3fb7c93b503d817b
El_Reng <leonelrojo7@gmail.com>
2026-04-20 19:34:43 -0300

    Se añadio la etiqueta "Recordatorios" a cada endpoint para una mejor documentación

--- main.py
+++ main.py
@@ -11,24 +11,24 @@ recordatorios = [
     {"id": 3, "titulo": "Ver ultima clase de Diseño", "fecha": "23/04"},
 ]
 
-@app.get("/recordatorios")
+@app.get("/recordatorios", tags = ["Recordatorios"])
 async def ver_recordatorios():
     return recordatorios
 
-@app.get("/recordatorio/{id}")
+@app.get("/recordatorio/{id}", tags = ["Recordatorios"])
 def ver_recodatorio(id: int):
     for r in recordatorios:
         if r["id"] == id:
            return r
     return []
 
-@app.post("/agregar-recordatorio")
+@app.post("/agregar-recordatorio", tags = ["Recordatorios"])
 def agregar_recordatorio(id: int, titulo: str, fecha: str):
     recordatorio_nuevo = {"id": id, "titulo": titulo, "fecha": fecha} 
     recordatorios.append(recordatorio_nuevo)
     return recordatorios
 
-@app.put("/reescribir-recordatorio")
+@app.put("/reescribir-recordatorio", tags = ["Recordatorios"])
 def reescribir_recordatorio(id: int = Body(), titulo: str = Body(), fecha: str = Body()):
     for r in recordatorios:
         if r["id"] == id:
@@ -37,7 +37,7 @@ def reescribir_recordatorio(id: int = Body(), titulo: str = Body(), fecha: str =
             return {"msg": "El recordatorio fue editado correctamente"}
     return []
 
-@app.patch("/editar-recordatorio")
+@app.patch("/editar-recordatorio", tags = ["Recordatorios"])
 def editar_recordatorio(id: int, titulo: str | None = None, fecha: str | None = None):
     for r in recordatorios:
         if r["id"] == id:
@@ -46,7 +46,7 @@ def editar_recordatorio(id: int, titulo: str | None = None, fecha: str | None = 
             return r
     return {"msg": "No se encontró el recordatorio a editar"}
 
-@app.delete("/borrar-recordatorio/{id}")
+@app.delete("/borrar-recordatorio/{id}", tags = ["Recordatorios"])
 def borrar_recordatorio(id: int):
     for r in recordatorios:
         if r["id"] == id:
